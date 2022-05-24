9837efbae736db5fb526b9f60d4ae4982143d3e0
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-24 20:46:06 +0530

    24c

--- JavaScript-Playground/JS-Projects/Guess-The-Number/script.js
+++ JavaScript-Playground/JS-Projects/Guess-The-Number/script.js
@@ -3,7 +3,8 @@ const body_Tag = Array.from(document.getElementsByTagName("body"))[0];
 // Creating flex-box for Again button and the hint "Between 1 and 20"
 const flex_box1 = document.createElement("div");
 flex_box1.setAttribute("class", "top-flex");
-flex_box1.innerHTML = "<span><button>Again!</button></span>";
+flex_box1.innerHTML =
+  "<button>Again!</button><span>(Between 1 to 20)</span>";
 body_Tag.appendChild(flex_box1);
 
 // Guess The Number! Heading
